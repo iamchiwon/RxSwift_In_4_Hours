85e3b29b431b5da37e371c1d6cadbc04abcd0ba9
iamchiwon <iamchiwon@itsdcode.com>
2018-12-21 14:12:12 +0900

    add step2 project

---
+++ Example/step2/Pods/Target Support Files/Pods-RxSwiftIn4Hours/Pods-RxSwiftIn4Hours-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftIn4Hours : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftIn4Hours
+@end
---
+++ Example/step2/RxSwiftIn4Hours/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  RxSwiftIn4Hours
+//
+//  Created by iamchiwon on 21/12/2018.
+//  Copyright © 2018 n.code. All rights reserved.
+//
+
+import RxSwift
+import UIKit
+
+class ViewController: UITableViewController {
+    @IBOutlet var imageView: UIImageView!
+
+    var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func exJust1() {
+        Observable.just("Hello World")
+            .subscribe(onNext: { str in
+                print(str)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    @IBAction func exJust2() {
+        Observable.just(["Hello", "World"])
+            .subscribe(onNext: { arr in
+                print(arr)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    @IBAction func exFrom1() {
+        Observable.from(["RxSwift", "In", "4", "Hours"])
+            .subscribe(onNext: { str in
+                print(str)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    @IBAction func exMap1() {
+        Observable.just("Hello")
+            .map { str in "\(str) RxSwift" }
+            .subscribe(onNext: { str in
+                print(str)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    @IBAction func exMap2() {
+        Observable.from(["with", "곰튀김"])
+            .map { $0.count }
+            .subscribe(onNext: { str in
+                print(str)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    @IBAction func exFilter() {
+        Observable.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            .filter { $0 % 2 == 0 }
+            .subscribe(onNext: { n in
+                print(n)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    @IBAction func exMap3() {
+        Observable.just("800x600")
+            .map { $0.replacingOccurrences(of: "x", with: "/") }
+            .map { "https://picsum.photos/\($0)/?random" }
+            .map { URL(string: $0) }
+            .filter { $0 != nil }
+            .map { $0! }
+            .map { try Data(contentsOf: $0) }
+            .map { UIImage(data: $0) }
+            .subscribe(onNext: { image in
+                self.imageView.image = image
+            })
+            .disposed(by: disposeBag)
+    }
+}
