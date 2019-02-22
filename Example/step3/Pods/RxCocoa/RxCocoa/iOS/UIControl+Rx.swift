18456d8b7649fa792afe65c8ce08d1d49fc18ebe
iamchiwon <iamchiwon@itsdcode.com>
2019-02-22 19:55:42 +0900

    pod update for step1, 2, 3

--- Example/step3/Pods/RxCocoa/RxCocoa/iOS/UIControl+Rx.swift
+++ Example/step3/Pods/RxCocoa/RxCocoa/iOS/UIControl+Rx.swift
@@ -43,8 +43,7 @@ extension Reactive where Base: UIControl {
                     return Disposables.create()
                 }
 
-                let controlTarget = ControlTarget(control: control, controlEvents: controlEvents) {
-                    control in
+                let controlTarget = ControlTarget(control: control, controlEvents: controlEvents) { _ in
                     observer.on(.next(()))
                 }
 
@@ -63,7 +62,7 @@ extension Reactive where Base: UIControl {
     public func controlProperty<T>(
         editingEvents: UIControlEvents,
         getter: @escaping (Base) -> T,
-        setter: @escaping (Base, T) -> ()
+        setter: @escaping (Base, T) -> Void
     ) -> ControlProperty<T> {
         let source: Observable<T> = Observable.create { [weak weakControl = base] observer in
                 guard let control = weakControl else {
@@ -93,7 +92,7 @@ extension Reactive where Base: UIControl {
     internal func controlPropertyWithDefaultEvents<T>(
         editingEvents: UIControlEvents = [.allEditingEvents, .valueChanged],
         getter: @escaping (Base) -> T,
-        setter: @escaping (Base, T) -> ()
+        setter: @escaping (Base, T) -> Void
         ) -> ControlProperty<T> {
         return controlProperty(
             editingEvents: editingEvents,
