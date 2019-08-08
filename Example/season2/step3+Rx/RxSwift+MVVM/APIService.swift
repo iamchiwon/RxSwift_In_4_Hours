e1b05ebaf8b6839c639f3fbbb202636280d7d159
Chiwon Song <iamchiwon@gmail.com>
2019-08-09 00:18:15 +0900

    example3+rx 소스 추가

---
+++ Example/season2/step3+Rx/RxSwift+MVVM/APIService.swift
@@ -0,0 +1,46 @@
+//
+//  APIService.swift
+//  RxSwift+MVVM
+//
+//  Created by iamchiwon on 07/08/2019.
+//  Copyright © 2019 iamchiwon. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+let MenuUrl = "https://firebasestorage.googleapis.com/v0/b/rxswiftin4hours.appspot.com/o/fried_menus.json?alt=media&token=42d5cb7e-8ec4-48f9-bf39-3049e796c936"
+
+class APIService {
+    static func fetchAllMenusRx() -> Observable<Data> {
+        return Observable.create { emitter in
+            fetchAllMenus() { result in
+                switch result {
+                case let .success(data):
+                    emitter.onNext(data)
+                    emitter.onCompleted()
+                case let .failure(error):
+                    emitter.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    static func fetchAllMenus(onComplete: @escaping (Result<Data, Error>) -> Void) {
+        URLSession.shared.dataTask(with: URL(string: MenuUrl)!) { data, res, err in
+            if let err = err {
+                onComplete(.failure(err))
+                return
+            }
+            guard let data = data else {
+                let httpResponse = res as! HTTPURLResponse
+                onComplete(.failure(NSError(domain: "no data",
+                                            code: httpResponse.statusCode,
+                                            userInfo: nil)))
+                return
+            }
+            onComplete(.success(data))
+        }.resume()
+    }
+}
