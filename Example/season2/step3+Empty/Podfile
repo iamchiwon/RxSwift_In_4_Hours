6b833e5abea3877f92f37947b58563b7053e5a02
iamchiwon <iamchiwon@itsdcode.com>
2019-10-24 00:31:16 +0900

    example added ; Season2 > step3+Empty

---
+++ Example/season2/step3+Empty/Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxSwift+MVVM' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RxSwift+MVVM
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'RxViewController'
+
+end
---
+++ Example/season2/step3+Empty/RxSwift+MVVM/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  RxSwift+MVVM
+//
+//  Created by iamchiwon on 05/08/2019.
+//  Copyright © 2019 iamchiwon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
